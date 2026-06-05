e0ec6299b76fd27437b2a32aabfd7a556c1afff3
adriancastro8646-cmky <adriancastro8646@gmail.com>
2026-06-04 22:52:34 -0600

    Se agrego el mensaje de bienvenida al Proyecto Final de Java

--- Proyecto.java
+++ Proyecto.java
@@ -2,5 +2,6 @@ import javax.swing.JOptionPane;
 public class Proyecto {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Proyecto Final!!");
+        JOptionPane.showMessageDialog(null, "¡Bienvenido al proyecto final de Java!");
     }
 }
