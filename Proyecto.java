26f985cb5a1111a3bd3d406c56b5b99e04e02177
adriancastro8646-cmky <adriancastro8646@gmail.com>
2026-06-11 18:31:19 -0600

    Se elimino el tercer repositorio

--- Proyecto.java
+++ Proyecto.java
@@ -3,7 +3,6 @@ public class Proyecto {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Proyecto Final!!");
         JOptionPane.showMessageDialog(null, "¡Bienvenido al proyecto final de Java!");
-        JOptionPane.showMessageDialog(null, "En este proyecto, aplicaremos los conceptos aprendidos a lo largo del curso.");
 
     }
 }
