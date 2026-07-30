7a15995515996856f8940bb1f7dc6d58ce6b5076
Adrián Castro <adriancastro8646@gmail.com>
2026-07-29 22:25:51 -0600

    Agregadas las clases DetalleVenta e Inventario

--- Proyecto.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-public class Proyecto {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Proyecto Final!!");
-        JOptionPane.showMessageDialog(null, "¡Bienvenido al proyecto final de Java!");
-
-    }
-}
